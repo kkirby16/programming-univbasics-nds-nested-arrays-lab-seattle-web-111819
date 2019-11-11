45eec673e103d5950305a2a783e1b87ead953762
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-11 04:54:01 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,7 +29,6 @@ def array_literal_matrix
   ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"], 
   ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
   ]
-  
   array_literal_matrix.sort
   
   # Using Array literal syntax only, build a nested array that uses the data in
