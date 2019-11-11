62fb56610362f424bd7afa1d1cf427981933e8b6
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-11 04:59:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,6 +29,7 @@ array_literal_matrix = [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
 end
 
 def matrix_lookup(matrix, row, column)
+  matrix[row][column]
   # Return the matrix's content at row and column
 end
 
